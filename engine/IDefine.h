30ba63eaccc866b9eb5142259bc8c3b646abe885
INTRANET\tianhaoli <odolgtt@sina.com>
2020-05-29 14:35:39 +0800

    first commit

---
+++ engine/IDefine.h
@@ -0,0 +1,20 @@
+#ifndef  __IDEFINE_H
+#define   __IDEFINE_H
+
+//宏定义 ：预编译阶段字符替换
+//类型定义typdef:重命名，对于编译器
+
+#ifdef ____WIN32_
+
+#include <WinSock2.h>;
+#pragma  comment(lib,"ws2_32");
+
+#endif // ____WIN32
+
+
+
+
+
+
+
+#endif // !__DEFINE_H
